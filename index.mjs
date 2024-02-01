424b6558e7794683aeadb8182741466c65b1a600
jatinbedii <jbedi432@gmail.com>
2024-02-01 18:59:35 +0530

    .env added

--- index.mjs
+++ index.mjs
@@ -1,3 +1,4 @@
+import "dotenv/config";
 import express from "express";
 import { Server } from "socket.io";
 import { createServer } from "http";
@@ -5,7 +6,7 @@ const app = express();
 const httpserver = createServer(app);
 const io = new Server(httpserver, {
   cors: {
-    origin: ["http://localhost:3000"],
+    origin: [process.env.FRONTEND],
   },
 });
 
@@ -31,6 +32,6 @@ io.on("connection", (socket) => {
   });
 });
 
-httpserver.listen(3001, () => {
+httpserver.listen(process.env.PORT, () => {
   console.log("app is listening");
 });
